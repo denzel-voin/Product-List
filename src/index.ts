0fb39a38b927eb9afacef9dba30c7647aa13a975
Denis Voinovich <denzelvojn@gmail.com>
2024-06-13 19:12:09 +0300

    done simple style and render

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import './sass/styles.sass';
 import './components/api'
 import { getProducts } from './components/api';
+import { Product } from './components/product';
 
 async function displayProducts() {
-	const products = await getProducts();
-	console.log(products);
+	const products :Product[] = await getProducts();
+	const productListElement :HTMLElement = document.getElementById('product-list');
+
+	if (productListElement) {
+		productListElement.innerHTML = '';
+
+		products.forEach(product => {
+			const productElement :HTMLDivElement = document.createElement('div');
+			productElement.className = 'product';
+
+			productElement.innerHTML = `
+                <img src="${product.image}" alt="${product.title}">
+                <h2>${product.title}</h2>
+                <p>${product.price} USD</p>
+            `;
+
+			productListElement.appendChild(productElement);
+		});
+	}
 }
 
 displayProducts();
